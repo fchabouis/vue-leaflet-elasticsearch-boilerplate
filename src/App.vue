aa314ac07919abe1d0069b4b86e07e61442992de
Francis Chabouis <francis.chabouis@interieur.gouv.fr>
2017-09-19 14:06:25 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <theMap></theMap>
+  </div>
+</template>
+
+<script>
+import theMap from './components/map'
+
+export default {
+  name: 'app',
+  components: {
+    theMap
+  }
+}
+</script>
+
+<style>
+  html, body, #app {
+    height: 100%;
+    width: 100%;
+    margin: 0px;
+  }
+</style>
---
+++ src/components/map.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="myMap">
+    </div>
+</template>
+
+<script>
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+      map: null,
+      tileLayer: null
+    }
+  },
+  mounted () {
+    this.map = L.map('myMap').setView([45.853459, 2.349312], 6)
+
+    this.tileLayer = L.tileLayer('http://osm.psi.minint.fr/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+      maxZoom: 18
+    }).addTo(this.map)
+  }
+}
+</script>
+
+<style>
+#myMap {
+     height: 100%;
+     width: auto;
+}
+</style>
