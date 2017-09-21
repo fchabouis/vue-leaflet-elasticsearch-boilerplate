4b8fc2d08ec23d1db718b22a55c05f3c7dbab2b5
Francis Chabouis <francis.chabouis@interieur.gouv.fr>
2017-09-21 14:24:23 +0200

    use in map.vue information coming from the config file and the store

--- src/components/map.vue
+++ src/components/map.vue
@@ -7,21 +7,35 @@
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
+import config from '../assets/config/config.json'
+import { mapGetters, mapState } from 'vuex'
+
 export default {
-  components: {
-  },
   data () {
     return {
       map: null,
       tileLayer: null
     }
   },
+  watch: {
+    basemap () {
+      this.tileLayer.setUrl(this.basemapUrl)
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'basemapUrl',
+      'basemapAttribution'
+    ]),
+    ...mapState([
+      'basemap'
+    ])
+  },
   mounted () {
-    this.map = L.map('myMap').setView([45.853459, 2.349312], 6)
+    this.map = L.map('myMap', {zoomControl: true}).setView(config.mapInitialView.center, config.mapInitialView.zoom)
 
-    this.tileLayer = L.tileLayer('http://osm.psi.minint.fr/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-      maxZoom: 18
+    this.tileLayer = L.tileLayer(this.basemapUrl, {
+      attribution: this.basemapAttribution
     }).addTo(this.map)
   }
 }
