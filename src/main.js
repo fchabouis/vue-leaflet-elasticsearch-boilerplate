8d56853572f34bf79040106d3b7296801004e04c
Francis Chabouis <francis.chabouis@interieur.gouv.fr>
2017-09-26 11:50:51 +0200

    add package vuex-router-sync

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import { sync } from 'vuex-router-sync'
 
 Vue.config.productionTip = false
+
+sync(store, router)
 
 /* eslint-disable no-new */
 new Vue({
