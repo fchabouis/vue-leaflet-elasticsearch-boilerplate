76d87fa0ce1da9700ebbf4ad4c10e30e0a8f7f3a
Francis Chabouis <francis.chabouis@interieur.gouv.fr>
2017-09-26 11:58:28 +0200

    add boundaries display and corresponding url routing

--- src/components/map.vue
+++ src/components/map.vue
@@ -16,21 +16,56 @@ export default {
   data () {
     return {
       map: null,
-      tileLayer: null
+      tileLayer: null,
+      boundariesLayerGroup: L.featureGroup()
     }
   },
   watch: {
+    '$route' (to, from) {
+      this.$store.dispatch('set_view')
+    },
     basemap () {
       this.tileLayer.setUrl(this.basemapUrl)
+    },
+    view () {
+      this.displayBoundaries()
+    }
+  },
+  methods: {
+    displayBoundaries () {
+      let vm = this
+      this.boundariesLayerGroup.clearLayers()
+      this.boundariesLayerGroup.addLayer(L.geoJson(this.boundaries, {
+        onEachFeature (feature, layer) {
+          layer.on('click', function () {
+            let route = {
+              name: 'view_with_id',
+              params: { view: vm.view.linksTo, id: feature.properties[vm.view.boundaries.idField] }
+            }
+            vm.$router.push(route)
+          })
+        },
+        filter (feature, layer) {
+          if (vm.view.boundaries.filter.activated) {
+            let idField = vm.view.boundaries.filter.idField
+            return feature.properties[idField] === vm.view.id
+          } else {
+            return true
+          }
+        }
+      }))
+      this.map.fitBounds(this.boundariesLayerGroup.getBounds())
     }
   },
   computed: {
     ...mapGetters([
       'basemapUrl',
-      'basemapAttribution'
+      'basemapAttribution',
+      'view'
     ]),
     ...mapState([
-      'basemap'
+      'basemap',
+      'boundaries'
     ])
   },
   mounted () {
@@ -41,6 +76,8 @@ export default {
     }).addTo(this.map)
 
     L.control.sidebar('left-sidebar').addTo(this.map)
+    this.map.addLayer(this.boundariesLayerGroup)
+    this.$store.dispatch('set_view')
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,28 +2,52 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import config from '../assets/config/config.json'
-import theMap from '../components/map'
+import { views } from '../assets/config/views.js'
+import fieldsName from '../assets/config/fieldsName.json'
+// import theMap from '../components/map'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: { theMap },
+//  modules: { theMap },
   strict: process.env.NODE_ENV !== 'production',
   state: {
+    boundaries: {},
     basemap: Object.keys(config.basemaps)[0]
   },
   mutations: {
     set_basemap (state, basemap) {
       state.basemap = basemap
+    },
+    set_boundaries (state, geojson) {
+      state.boundaries = geojson
     }
   },
-  actions: {},
+  actions: {
+    set_view (context) {
+      let view = context.getters.view
+      context.commit('set_boundaries', view.boundaries.geojson)
+    }
+  },
   getters: {
     basemapUrl (state) {
       return config.basemaps[state.basemap].url
     },
     basemapAttribution (state) {
       return config.basemaps[state.basemap].attribution
+    },
+    view (state) {
+      let viewName = state.route.params.view || Object.keys(views)[0]
+      let id = state.route.params.id || null
+      let view = views[viewName]
+      view.id = id
+      view.boundaries.idField = fieldsName.geojson[view.boundaries.name].id
+      view.boundaries.displayNameField = fieldsName.geojson[view.boundaries.name].displayName
+      if (view.boundaries.filter.activated) {
+        view.boundaries.filter.idField = fieldsName.geojson[view.boundaries.filter.filterOn].id
+      }
+
+      return view
     }
   }
 })
